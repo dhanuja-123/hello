1ab804896e6b9611782b800e8e2909bad45bd4a3
dhanuja-123 <139104946+dhanuja-123@users.noreply.github.com>
2023-07-24 16:32:52 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class hello
 {
 public static void main(String args[])
 {
-System.out.println("hi , sapient");
-System.out.print(3+4);
+System.out.println("hi , welcome to sapient");
+System.out.print(5+5);
 }
 }
